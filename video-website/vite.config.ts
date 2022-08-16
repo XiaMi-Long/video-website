37070e82a1b379825601fae52695230707ba73ff
weiwenyu <2454150875@qq.com>
2022-08-16 17:34:44 +0800

    init packget

---
+++ video-website/vite.config.ts
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: wwy
+ * @Date: 2022-08-16 17:06:43
+ * @LastEditors: wwy
+ * @LastEditTime: 2022-08-16 17:27:52
+ */
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()]
+})
